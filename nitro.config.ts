74d099eb978899f94393a4486a2b7825be12cceb
Aditya <dashing4149@gmail.com>
2025-12-03 00:21:31 +0530

    add bun runtime for vercel

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,10 +3,15 @@ import { defineNitroConfig } from "nitro/config";
 export default defineNitroConfig({
     compatibilityDate: "2025-07-15",
     preset: "vercel",
+    vercel: {
+        functions: {
+            runtime: "bun1.x"
+        }
+    },
     minify: true,
     compressPublicAssets: true,
     prerender: {
         routes: ["/", "/leaderboard", "/failures", "/analytics", "/chat"],
     },
-    
+
 })
